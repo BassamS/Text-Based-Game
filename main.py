0bc3bd1f1c7bf779f7f77d14800daaa32a46ffa7
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-26 08:43:56 +0200

    Not enough balance

--- main.py
+++ main.py
@@ -52,8 +52,16 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    total_bet = bet * lines
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(
+                f"You don't have enough to bet that amount, your current balance is: {balance}")
+        else:
+            break
+
     print(
         f'You are betting {bet} on {lines} lines. Total bet is equal to: {total_bet}.')
 
